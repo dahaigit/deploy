4bcad2daeed7dac7cae6ae0d96a60c3b64a3fef4
menghailong <menghailong@xiyuemedia.com>
2018-09-21 19:19:32 +0800

    'envoy'

---
+++ Envoy.blade.php
@@ -0,0 +1,8 @@
+@servers(['web-1' => 'root@192.168.0.38', 'web-2' => 'root@192.168.0.199'])
+
+@task('deploy', ['on' => ['web-1', 'web-2', 'parallel' => true]])
+    cd /var/www/deploy
+    git pull origin master
+    composer install --no-dev
+    php artisan migrate --force
+@endtask

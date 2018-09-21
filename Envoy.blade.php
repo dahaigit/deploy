f7374e655e7051375c21260eda364073f91dd167
menghailong <menghailong@xiyuemedia.com>
2018-09-21 20:48:38 +0800

    bug

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,4 +1,4 @@
-@servers(['web-1' => 'dahai@192.168.0.38', 'web-2' => 'dahai@192.168.0.199'])
+@servers(['web-1' => 'root@192.168.0.38', 'web-2' => 'root@192.168.0.199'])
 
 @task('deploy', ['on' => ['web-1', 'web-2'], 'parallel' => true])
 cd /var/www/deploy

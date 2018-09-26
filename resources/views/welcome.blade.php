898ded76cf2d926ed1d2f64f4d8670f082d9b8cc
menghailong <menghailong@xiyuemedia.com>
2018-09-26 18:27:11 +0800

    去掉google字体

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,6 @@
 
         <title>Laravel envoy deploy</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
         <!-- Styles -->
         <style>
             html, body {
